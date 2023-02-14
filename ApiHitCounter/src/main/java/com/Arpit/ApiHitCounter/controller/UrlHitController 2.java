8e88dcd9a80b896c5c68895e7780082b4cc68e8a
Arpitjain4454 <67001521+Arpitjain4454@users.noreply.github.com>
2023-02-14 14:34:55 +0530

    Add files via upload

---
+++ ApiHitCounter/src/main/java/com/Arpit/ApiHitCounter/controller/UrlHitController 2.java
@@ -0,0 +1,27 @@
+package com.arpitj.ApiHitCounter.controller;
+
+import com.arpitj.ApiHitCounter.service.UrlHitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/visitors-count")
+class UrlHitController {
+
+    @Autowired
+    private UrlHitService urlService;
+
+    public UrlHitController(UrlHitService urlService) {
+        this.urlService=urlService;
+    }
+
+    @GetMapping("/username/{username}/count")
+    public String getCount(@PathVariable String username) {
+        String visit=urlService.getVisits(username);
+//        System.out.println(visit.getCount() + " " + visit.getName());
+        return visit.toString();
+    }
+}
---
+++ ApiHitCounter/src/test/java/com/arpit/ApiHitCounter/ApiHitCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpit.ApiHitCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHitCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
